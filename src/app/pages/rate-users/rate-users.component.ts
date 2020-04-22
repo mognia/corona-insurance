9420f1e3af38951202eb12d1361bad4189add513
mognia <1mognia@gmail.com>
2020-04-22 15:46:29 +0430

    vote user connected

--- src/app/pages/rate-users/rate-users.component.ts
+++ src/app/pages/rate-users/rate-users.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {vote} from 'corona-interface/ethereum'
 @Component({
   selector: 'app-rate-users',
   templateUrl: './rate-users.component.html',
   styleUrls: ['./rate-users.component.css']
 })
 export class RateUsersComponent implements OnInit {
-
+  voteForm = new FormGroup({
+    value: new FormControl('', [Validators.max(100), Validators.required]),
+    address: new FormControl('', [ Validators.required]),
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  vote() {
+    if (this.voteForm.valid) {
+      vote(this.voteForm.value)
+    }
+  }
 }
