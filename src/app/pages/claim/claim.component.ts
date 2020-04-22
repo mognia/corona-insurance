99b22bae194cbf10bea96ae1abdacf1b004137f9
mognia <1mognia@gmail.com>
2020-04-22 16:33:20 +0430

    calim connected

--- src/app/pages/claim/claim.component.ts
+++ src/app/pages/claim/claim.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {claim} from 'corona-interface/ethereum'
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-claim',
   templateUrl: './claim.component.html',
   styleUrls: ['./claim.component.css']
 })
 export class ClaimComponent implements OnInit {
-
+  claimForm = new FormGroup({
+    address: new FormControl('', [ Validators.required]),
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  claim(){
+    if (this.claimForm.valid){
+      claim(this.claimForm.value)
+    }
+  }
 }
